4c9553d8d0cf358f168715dcb347715e87efcb79
Marissa Douglass <mahdouglass@gmail.com>
2020-07-02 14:23:14 -0400

    Day 1

---
+++ day_1.js
@@ -0,0 +1,14 @@
+// Write a function that returns the sum of two numbers
+
+function addTwoParams(a, b) {
+    let added = parseInt(a) + parseInt(b);
+    return added;
+}
+
+// Write a functions that returns the sum of all numbers regardless of the number of parameters.
+
+function addAllNumbers(...nums) {
+    return nums.reduce((a, b) => {
+        return a + b;
+    });
+}
